84cb94193e142f0e2c591f81bba2d6ec52c200b6
sreeav <sreeav.6@gmail.com>
2023-08-15 19:58:04 +0530

    Merging concept

--- simple.sh
+++ simple.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#The below f1,f2 are created in master branch
 touch f1
 git add f1
 git commit -m "a"
@@ -7,6 +9,7 @@ touch f2
 git add f2
 git commit -m "b"
 
+#Here we are creating a new branch name called Test and staged t1,t2,t3 file and committed
 git checkout -b Test
 touch t1
 git add t1
@@ -20,12 +23,14 @@ touch t3
 git add t3
 git commit -m "e"
 
+#Here we are again checking out to the master branch and created a file called f3 and staged and then commited
 git checkout master
 git log --oneline
 touch f3
 git add f3
 git commit -m "f"
 
+#Here we are using a concept called Merge<we need to checkout to Master branch and then do merge of the child branch
 git merge Test
 git log --oneline
-test
+
